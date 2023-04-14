4b6374168b92ce72d65a28b602b1a11e49f1db98
joykara <joykarani1@gmail.com>
2023-04-14 20:41:50 +0300

    styles

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-window.addEventListener('scroll', onScroll);
 
-function onScroll(){
-    var onScrolls = document.querySelectorAll('.onScroll');
-
-    for(var i = 0; i < onScrolls.length; i++){
-
-        var windowheight = window.innerHeight;
-        var onScrolltop = onScrolls[i].getBoundingClientRect().top;
-        var onScrollpoint = 10;
-
-        if(onScrolltop < windowheight - onScrollpoint){
-          onScrolls[i].classList.add('active');
-        }
-        else{
-          onScrolls[i].classList.remove('active');
-        }
-      }
-}
